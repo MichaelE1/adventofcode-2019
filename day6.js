40f1c056d79504ea02e4cf87e35f9d550cb1e325
MichaelE1 <mickel13@gmail.com>
2019-12-16 20:45:43 +1000

    day 6

---
+++ day6.js
@@ -0,0 +1,55 @@
+/* Part One */
+
+// const input = require("./inputs/day6-input");
+
+// const orbits = input.reduce((orbits, line) => {
+//   const [object1, object2] = line.split(")");
+//   orbits[object2] = object1;
+//   return orbits;
+// }, {});
+
+// let totalOrbits = 0;
+// const objectsOrbitingAnother = Object.keys(orbits);
+
+// for (let i = 0; i < objectsOrbitingAnother.length; i++) {
+//   const currentObject = objectsOrbitingAnother[i];
+//   let nextObject = orbits[currentObject];
+
+//   while (nextObject) {
+//     nextObject = orbits[nextObject];
+//     totalOrbits++;
+//   }
+// }
+
+// console.log(totalOrbits);
+
+/* Part Two */
+
+const input = require("./inputs/day6-input");
+
+const allOrbits = input.reduce((orbits, line) => {
+  const [object1, object2] = line.split(")");
+  orbits[object2] = object1;
+  return orbits;
+}, {});
+
+const getOrbitedObjects = orbiter => {
+  if (orbiter in allOrbits) {
+    return [allOrbits[orbiter], ...getOrbitedObjects(allOrbits[orbiter])];
+  }
+  return [];
+};
+
+const orbitedByYou = getOrbitedObjects("YOU");
+const orbitedBySan = getOrbitedObjects("SAN");
+
+for (let i = 0; i < orbitedByYou.length; i++) {
+  const index = orbitedBySan.findIndex(
+    satellite => satellite === orbitedByYou[i]
+  );
+
+  if (index !== -1) {
+    console.log(index + i);
+    break;
+  }
+}
